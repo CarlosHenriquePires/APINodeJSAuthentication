f46cb8d00961b111a07219df2a4abfe38aa21a87
carluxhenrique <carluxhenrique@gmail.com>
2020-06-02 23:23:09 -0300

    Criando o hash da senha com bcrypt

--- routes/authentication.js
+++ routes/authentication.js
@@ -1,6 +1,8 @@
 var router = require('express').Router();
 var usuarioModel = require('../models/databaseModel');
 var validandoCadastro = require('../validandoCampos/validandoCadastro');
+var validandoLogin = require('../validandoCampos/validandoLogin');
+var bcrypt = require('bcrypt');
 
 // Rota para cadastro de usuário
 router.post('/cadastrar', async function(req,res){
@@ -13,11 +15,16 @@ router.post('/cadastrar', async function(req,res){
     var emailExiste = await usuarioModel.findOne({email: req.body.email});
     if(emailExiste) return res.status(400).send("E-mail já existe!");
 
-    // Se passar pela validação o usuário é registrado no banco de dados
+    // Criando o hash da senha
+    var salt = await bcrypt.genSalt(10);
+    var senhaHash = await bcrypt.hash(req.body.senha, salt);
+    
+
+    // Se passar pela validação o usuário é registrado no banco de dados com senha encriptada
     var usuario = new usuarioModel({
         nome: req.body.nome,
         email: req.body.email,
-        senha: req.body.senha
+        senha: senhaHash
     });
     try {
         var salvarUsuario = await usuario.save();
@@ -31,25 +38,13 @@ router.post('/cadastrar', async function(req,res){
 router.post('/login', async function(req,res){
 
     // Receber o retorno de um erro caso haja
-    var {error} = validandoCadastro(req.body);
+    var {error} = validandoLogin(req.body);
     if(error) return res.status(400).send(error.details[0].message); // Enviar o error
 
     // Verificando se o e-mail não existe
     var emailExiste = await usuarioModel.findOne({email: req.body.email});
     if(!emailExiste) return res.status(400).send("E-mail ou senha incorreta!");
 
-    // Se passar pela validação o usuário é registrado no banco de dados
-    var usuario = new usuarioModel({
-        nome: req.body.nome,
-        email: req.body.email,
-        senha: req.body.senha
-    });
-    try {
-        var salvarUsuario = await usuario.save();
-        res.send(salvarUsuario);
-    } catch (error) {
-        res.status(400).send(error);        
-    }
 });
 
 module.exports = router;
